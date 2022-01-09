6a7587050788a6fc2f62c27f6ee493bcc8519bbc
João Amadeu <jmamadeu2000@gmail.com>
2022-01-09 23:50:35 +0100

    chore: setup the repo

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import LogoSVG from "./assets/images/logo"
+
+
+function App() {
+
+  return (
+    <main>
+      <header className="bg-black-100 h-24">
+        <LogoSVG />
+
+        
+      </header>
+    </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: { 100: "#202020" },
+      }
+    },
+  },
+  plugins: [],
+}
