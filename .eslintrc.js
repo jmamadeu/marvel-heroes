088ccaf8aca90315891d625c99ee3bf350aa2562
João Amadeu <jmamadeu2000@gmail.com>
2022-01-10 02:51:02 +0100

    chore: configure path alias

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["@callstack/eslint-config/react", "plugin:react/jsx-runtime"],
+  rules: {
+    "import/no-unresolved": "off",
+    "react-native/no-raw-text": "off"
+  }
+};
