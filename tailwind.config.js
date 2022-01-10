3f79e6bdd130db6112c3f9fd942bf760791a9d8b
João Amadeu <jmamadeu2000@gmail.com>
2022-01-10 18:10:30 +0100

    chore: add search banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
     extend: {
       colors: {
         black: { 100: "#202020" },
+        gray: { 10: "#f5f5f5", 20: "#858585" }
+      },
+      backgroundImage: {
+        'marvel': "url('./src/assets/images/marvel-background.png')",
       }
     },
   },
